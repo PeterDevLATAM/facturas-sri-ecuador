ec5523cafacd99d9fc0fe6a217e713582fa0c148
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-05-18 10:23:01 -0500

    comercial name added to the table

--- index.js
+++ index.js
@@ -14,39 +14,43 @@ files.map((file) => {
       // Extract inner XML data
       try {
         parseString(result.autorizacion.comprobante, function (err, result) {
-        const fechaEmision = result.factura.infoFactura[0].fechaEmision[0];
-        let dirEstablecimiento = "NO DIR";
-        if (result.factura.infoFactura[0].dirEstablecimiento) {
-          dirEstablecimiento =
-            result.factura.infoFactura[0].dirEstablecimiento[0];
-        }
-
-        let conIva = 0;
-        let sinIva = 0;
-        if (
-          result.factura.infoFactura[0].totalConImpuestos[0].totalImpuesto
-            .length > 1
-        ) {
-          sinIva = Number(
-            result.factura.infoFactura[0].totalConImpuestos[0].totalImpuesto[1]
+          const fechaEmision = result.factura.infoFactura[0].fechaEmision[0];
+          let nombreEstablecimiento = "NO NAME";
+          let dirEstablecimiento = "NO DIR";
+          let conIva = 0;
+          let sinIva = 0;
+         
+          if (result.factura.infoTributaria[0].razonSocial[0]) {
+            nombreEstablecimiento =
+              result.factura.infoTributaria[0].razonSocial[0];
+          }
+          if (result.factura.infoFactura[0].dirEstablecimiento) {
+            dirEstablecimiento =
+              result.factura.infoFactura[0].dirEstablecimiento[0];
+          }       
+          if (
+            result.factura.infoFactura[0].totalConImpuestos[0].totalImpuesto
+              .length > 1
+          ) {
+            sinIva = Number(
+              result.factura.infoFactura[0].totalConImpuestos[0]
+                .totalImpuesto[1].baseImponible[0]
+            );
+          }
+          conIva = Number(
+            result.factura.infoFactura[0].totalConImpuestos[0].totalImpuesto[0]
               .baseImponible[0]
           );
-        }
-        conIva = Number(
-          result.factura.infoFactura[0].totalConImpuestos[0].totalImpuesto[0]
-            .baseImponible[0]
-        );
-        const data = { fechaEmision, dirEstablecimiento, conIva, sinIva };
-        resultData.push(data);
-      });  
+          const data = { fechaEmision, nombreEstablecimiento, dirEstablecimiento, conIva, sinIva };
+          resultData.push(data);
+        });
       } catch (error) {
-          console.log(`File ${file} not recognized`)
+        console.log(`File ${file} not recognized`);
       }
-      
     });
   });
 });
 setTimeout(() => {
-    console.table(resultData)
+  console.table(resultData)
   fs.writeFileSync("data.xlsx", json2xls(resultData), "binary");
 }, 1000);
